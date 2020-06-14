11fc7e8d1c2ca6f4955ff558efee4ea46513e535
ShahadatPOM <naiem.3112@gmail.com>
2020-06-14 11:48:49 +0600

    category model, migration, controller add

---
+++ app/Category.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $guarded = [
+        'created_at', 'updated_at', 'deleted_at'
+    ];
+}
