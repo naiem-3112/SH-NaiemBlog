9906b373c0eb9b26f1bc3ca1fee45b794c842183
ShahadatPOM <naiem.3112@gmail.com>
2020-06-14 18:07:45 +0600

    tag model, migration, controller created

---
+++ resources/views/admin/tag/create.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.back_temp')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Create Category</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                        <li class="breadcrumb-item active"><a href="{{ route('category.index') }}">Category List</a>
+                        </li>
+                        <li class="breadcrumb-item active">Create Category</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h3 class="card-title">Create Category</h3>
+                                <a href="{{ route('category.index') }}" class="btn btn-primary">All Category</a>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body p-0">
+                            <div class="col-12 col-lg-6 offset-lg-3 col-md-8 offset-md-2 ">
+                                <form action="{{ route('category.store') }}" method="post">
+                                    @csrf
+                                    <div class="card-body">
+                                        <div class="form-group">
+                                            <label for="name">Category Name</label>
+                                            <input type="text" class="form-control" name="name" id="name" placeholder="Enter name">
+                                            @error('name')
+                                            <div class="alert alert-danger mt-2 mb-2">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="description">Description</label>
+                                            <textarea name="description" id="description" rows="4" class="form-control" placeholder="Enter Description"></textarea>
+                                            @error('description')
+                                            <div class="alert alert-danger mt-2 mb-2">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="card-footer">
+                                            <button type="submit" class="btn btn-lg btn-primary">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/admin/tag/index.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.back_temp')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Category List</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                        <li class="breadcrumb-item active">Category</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h3 class="card-title">Category List</h3>
+                                <a href="{{ route('category.create') }}" class="btn btn-primary">Create Category</a>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body p-0">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Slug</th>
+                                    <th>Post Count</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($categories as $key => $category)
+                                    <tr>
+                                        <td>{{$key+1}}</td>
+                                        <td>{{$category->name}}</td>
+                                        <td>{{$category->slug}}</td>
+                                        <td>{{$category->id}}</td>
+                                        <td>
+                                                <a href="{{ route('category.show', $category->id) }}" class="btn btn-xs btn-success"><i class="fas fa-eye"></i></a>
+                                                <a href="{{ route('category.edit', $category->id) }}" class="btn btn-xs btn-warning"><i class="fas fa-edit"></i></a>
+
+                                            <form action="{{ route('category.destroy', $category->id) }}" method="post" style="display: inline-block">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button onclick=" alert('Are You Sure TO DELETE!')" class="btn btn-xs btn-danger" type="submit"><i class="fas fa-trash"></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
