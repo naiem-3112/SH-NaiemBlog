4cc489f2e1ecc8af5526923c9234074ed29352b9
ShahadatPOM <naiem.3112@gmail.com>
2020-06-21 00:08:00 +0600

    user controller created and CRUD operation written complete

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Session;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::latest()->paginate(20);
+        return view('user.index', compact('user'));
+    }
+
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users, email',
+            'password' => 'required|min:8'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'description' => $request->description,
+            'image' => $request->image,
+            'password' => $request->password
+        ]);
+        Session::flash('success', 'user info stored successfully');
+        return redirect()->route('user.index');
+    }
+
+    public function show(User $user)
+    {
+        return view('user.show');
+    }
+
+    public function edit(User $user)
+    {
+        return view('user.edit', compact('user'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users, email',
+            'password' => 'required|min:8'
+        ]);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->description = $request->description;
+        $user->image = $request->image;
+        $user->password = $request->password;
+        $user->save();
+
+        Session::flash('success', 'user info updated successfully');
+        return redirect()->route('user.index');
+
+    }
+
+    public function destroy(User $user)
+    {
+        if($user){
+            $user->delete();
+            Session::flash('success', 'user info deleted successfully');
+        }
+        return redirect()->back();
+    }
+}
